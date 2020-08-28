41ccdb5837bd68278d41413f4826bf8cb807a872
legend <48868398+lzgznd@users.noreply.github.com>
2020-08-28 23:04:52 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client
+
+const prefix = '!';
+
+var version = 'v1.0';
+
+bot.on('ready', () => {
+    bot.user.setStatus('idle')
+    bot.user.setPresence({
+        game: {
+            name: '.',
+            type: "idle",
+            url: ""
+        }
+    });
+});
+
+//bot.login(process.env.BOT_TOKEN);
